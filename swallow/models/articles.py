417deaa8e458b425b2b08297945188518cad49a8
boyaziqi <catsaction@126.com>
2019-08-05 17:12:34 +0800

    add article category and tag

--- swallow/models/articles.py
+++ swallow/models/articles.py
@@ -17,3 +17,23 @@ class Article(db.Model):
 
     def __repr__(self):
         return f'<Article {self.title}>'
+
+
+class Category(db.Model):
+    """
+    文章分类 model
+    """
+    __tablename__ = 'article_categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+
+
+class Tag(db.Model):
+    """
+    文章标签 model
+    """
+    __tablename__ = 'article_tags'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)

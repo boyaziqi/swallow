dc9e10bc71e8cf0263fc581602ece1c935b9eb63
boyaziqi <catsaction@126.com>
2020-02-15 20:07:06 +0800

    add test

---
+++ tests/test_hello.py
@@ -0,0 +1,16 @@
+"""sample test"""
+from unittest import TestCase
+
+from swallow.utils import hello
+
+
+class TestHello(TestCase):
+    """sample test"""
+
+    def test_world(self):
+        """sample test"""
+        self.assertEqual(hello.hello('world'), 'hello world')
+
+    def test_world_unicode(self):
+        """sample test with unicode"""
+        self.assertEqual(hello.hello(u'world'), u'hello world')
